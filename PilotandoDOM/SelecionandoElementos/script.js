40d7aedb89648ac623e4839523fcb768360988e3
flanaomi <flavia_ando@hotmail.com>
2022-12-13 07:17:39 +0900

    Estudos sobre DOM

---
+++ PilotandoDOM/SelecionandoElementos/script.js
@@ -0,0 +1,24 @@
+//getElementById
+    
+const elementId = document.getElementById('blog-title')
+console.log(elementId)
+
+//getElementByClassName
+
+const elementClassName = document.getElementsByClassName('one')
+console.log(elementClassName)
+
+//getElementByTagName
+
+const elementTagName = document.getElementsByTagName ('meta')
+console.log(elementTagName)
+
+//querySelector
+
+const elementqueryselector = document.querySelector ('title')
+console.log(elementqueryselector)
+
+//querySelectorAll
+
+const elementqueryselectorAll = document.querySelectorAll ('[src] meta')
+console.log(elementqueryselectorAll)
