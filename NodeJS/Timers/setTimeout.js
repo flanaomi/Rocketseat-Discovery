df82cdabebc9907e36beaa3e5e1e138e37d1718e
flanaomi <flavia_ando@hotmail.com>
2023-02-16 21:05:14 +0900

    Timers em NodeJs

---
+++ NodeJS/Timers/setTimeout.js
@@ -0,0 +1,22 @@
+/* const timeOut = 3000
+const finished = () => console.log('função executada!')
+
+setTimeout(finished, timeOut)
+
+console.log('Mostrar') */
+
+const time1 = 1000
+const time2 = 2000
+const time3 = 3000
+
+const executar2 = () => console.log('Função executada segundo')
+
+const executar1 = () => console.log('Função executada primeiro')
+
+const executar3 = () => console.log('Função executada terceiro')
+
+setTimeout(executar3, time3)
+setTimeout(executar2, time2)
+setTimeout(executar1, time1)
+
+console.log("Executando funções atraves de timers")
